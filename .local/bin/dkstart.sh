8100e1cafbbcfc9530e9da4f7c2d3800c914988d
Ron Ivie <rwivie@gmail.com>
2023-10-28 23:20:44 -0500

    .local/bin/dkstart.sh

--- .local/bin/dkstart.sh
+++ .local/bin/dkstart.sh
@@ -9,6 +9,9 @@ picom --config ~/.config/picom/picom_dwm.conf  --daemon &
 (sleep 1; xautolock -time 10 -locker lock -corners '+-00' -cornerdelay 15 -notify 15 -notifier "notify-send 'Screen will lock in 15 s'") &
 caffeine &
 volumeicon &
+sleep 10 && mpdris2-rs &
+/usr/local/lib/libexec/kdeconnectd &
+sleep 5 && kdeconnect-indicator &
 
 exec ck-launch-session dk
 
