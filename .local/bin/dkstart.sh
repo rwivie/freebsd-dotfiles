292426ace735c7a1e6faf0c03392813280fdf0eb
Ron Ivie <rwivie@gmail.com>
2023-10-29 11:18:59 -0500

    .local/bin/dkstart.sh

--- .local/bin/dkstart.sh
+++ .local/bin/dkstart.sh
@@ -6,7 +6,7 @@ nitrogen --restore &
 numlockx on &
 dunst -conf ~/.config/dunst/dunstrc &
 picom --config ~/.config/picom/picom_dwm.conf  --daemon &
-(sleep 1; xautolock -time 10 -locker lock -corners '+-00' -cornerdelay 15 -notify 15 -notifier "notify-send 'Screen will lock in 15 s'") &
+(sleep 1; xautolock -time 10 -locker lock_dk -corners '+-00' -cornerdelay 15 -notify 15 -notifier "notify-send 'Screen will lock in 15 s'") &
 caffeine &
 volumeicon &
 sleep 10 && mpdris2-rs &
